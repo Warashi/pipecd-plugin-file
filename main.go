3f29adb1a2f21ebe20e013813a2a93da0f132dd3
Shinnosuke Sawada-Dazai <shin@warashi.dev>
2025-06-21 13:29:08 +0900

    feat: define configuration structs for plugin
    Signed-off-by: Shinnosuke Sawada-Dazai <shin@warashi.dev>

--- main.go
+++ main.go
@@ -6,8 +6,22 @@ import (
 	sdk "github.com/pipe-cd/piped-plugin-sdk-go"
 )
 
+type (
+	// We don't need plugin-wide config, so we can use an empty struct.
+	config struct{}
+	// We don't need deploy target config, so we can use an empty struct.
+	// When we need some configs like `targetMachine` or `targetKubernetesCluster`, we can add them here.
+	deployTargetConfig struct{}
+	// We can define the application config here.
+	// This config will be used to configure the application that this plugin will deploy.
+	applicationConfig struct {
+		// Path is the path to the destination directory where the files will be copied.
+		Path string `json:"path"`
+	}
+)
+
 func main() {
-	plugin, err := sdk.NewPlugin[any, any, any]("0.0.1")
+	plugin, err := sdk.NewPlugin[config, deployTargetConfig, applicationConfig]("0.0.1")
 	if err != nil {
 		log.Fatalln(err)
 	}
